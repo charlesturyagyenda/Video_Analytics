dd2a39c0a3b20ae36095e87ff7392c150bd602ba
charlesturyagyenda <turchaj@yahoo.com>
2016-12-05 14:26:30 +0000

    just testing

---
+++ Face_Detection_Video.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture()
+cap.open("FIG_2016.mp4")
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = cap.read()
+
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray,1.3, 8)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.imshow('Video Feed at FLE Instance....',frame)
+        cv2.waitKey(25)
+
+    else:
+        break
+
+cv2.destroyAllWindows()
+cap.release()

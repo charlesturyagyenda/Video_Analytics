8fcbc956c2f766fb0961ca886b566229378bc9df
charlesturyagyenda <charlesturyagyenda@users.noreply.github.com>
2016-12-16 14:37:33 +0000

    Update Face_Detection_Video.py

--- Face_Detection_Video.py
+++ Face_Detection_Video.py
@@ -14,13 +14,14 @@ while True:
     ret, frame = cap.read()
 
     if ret == True:
+        count +=1
+        print(count)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray,1.3, 8)
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         
-    count +=1
-    print(count)
+    
     else:
         break
 
